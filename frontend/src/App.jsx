42a939c348f40ab2eb084fed2f8f7b195fecec83
DarrenCT <99516347+DarrenCT@users.noreply.github.com>
2024-12-04 16:50:44 -0500

    Delete sidebar.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, useLocation, Outlet } from 'react-router-dom';
 import NavBar from './components/shared/NavBar';
-import Sidebar from './components/shared/Sidebar';
 import ProductCatalog from './components/pages/ProductCatalog';
 import CartPage from './components/pages/CartPage';
 import ProductDetailsPage from './components/pages/ProductDetailsPage';
@@ -15,19 +14,11 @@ import OrderConfirmationPage from './components/pages/OrderConfirmationPage';
 // General Layout Component
 const AppLayout = () => {
   const location = useLocation();
-  const showGeneralSidebar =
-    location.pathname !== '/' &&
-    location.pathname !== '/search' &&
-    location.pathname !== '/checkout' &&
-    location.pathname !== '/cart' &&
-    !location.pathname.startsWith('/order-confirmation') &&
-    !['/register', '/sign_in'].includes(location.pathname);
 
   return (
     <div className="min-h-screen bg-gray-50">
       {['/register', '/sign_in'].includes(location.pathname) || <NavBar />}
       <div className="flex">
-        {showGeneralSidebar && <Sidebar />}
         <main className="flex-1">
           <Outlet />
         </main>
